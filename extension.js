ae738197fe52285fb0e037d026285e08ff10dd0b
lihz <lihz@matrix.localdomain>
2023-10-19 16:19:28 +0800

    1. update new icon; 2.try to make sure icon/toggle/real status always keep consistence;

--- extension.js
+++ extension.js
@@ -38,20 +38,22 @@ class Indicator extends PanelMenu.Button {
         this.icon = new St.Icon();
         this.add_child(this.icon);
 
-        let menuItem = new PopupMenu.PopupSwitchMenuItem(
-            'Sound Card', this._soundcard_state());
-        menuItem.connect('toggled', this._onToggle.bind(this));
-        this.menu.addMenuItem(menuItem);
+        this.menuItem = new PopupMenu.PopupSwitchMenuItem(
+            'Sound Card', this._soundcard_status());
+        this.menuItem.connect('toggled', this._onToggle.bind(this));
 
-        this.menu.addMenuItem(menuItem);
-        this._update_icon();
+        this.menu.addMenuItem(this.menuItem);
+
+        this.last_status = null;
+        // init to correct status
+        this._update_all();
     }
 
     _log(msg) {
         log("[" + App.uuid + "_" + App.metadata.version + "]" + ": " + msg);
     }
 
-    _soundcard_state() {
+    _soundcard_status() {
         let cmd="ls -d /sys/class/sound/card0/";
         try {
             let [result, stdout, stderr, status] = GLib.spawn_command_line_sync(cmd);
@@ -62,42 +64,55 @@ class Indicator extends PanelMenu.Button {
         }
     }
 
-    _write_to_file(cmd) {
+   _write_command(cmd) {
         let proc = Gio.Subprocess.new(cmd, Gio.SubprocessFlags.STDIN_PIPE);
         proc.communicate_utf8_async("1", null, (proc, res) => {
             try{
-                let [, stdout,stderr] = proc.communicate_utf8_finish(res);
-                return true
+                let [ok, stdout,stderr] = proc.communicate_utf8_finish(res);
+                // delay 1s, than update icon and toggle state
+                // make sure the /sys/class/sound/card0/ dir show or disappear
+                GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
+                    this._update_all();
+                    return GLib.SOURCE_REMOVE;
+                })
             }catch(e){
                 logError(e);
-                return false
             }
         })
     }
 
     _update_icon(status) {
         let icon_status = status ? "enable": "disable";
-        let base_icon = App.path + '/icons/' + icon_status;             
-        let file_icon = Gio.File.new_for_path(base_icon + '.png')           
-        let icon = Gio.icon_new_for_string(file_icon.get_path());           
+        let base_icon = App.path + '/icons/' + icon_status;
+        let file_icon = Gio.File.new_for_path(base_icon + '.svg');
+        let icon = Gio.icon_new_for_string(file_icon.get_path());
         this.icon.set_gicon(icon);
+        this.icon.set_icon_size(26);
+    }
+
+    _update_toggle(status) {
+        this.menuItem.setToggleState(status);
+    }
+
+    _update_all() {
+        let status = this._soundcard_status();
+        this._update_icon(status);
+        this._update_toggle(status);
+        if (this.last_status !== null && this.last_status !== status) {
+            let msg = "Turned SoundCard " + (status ? "On": "Off");
+            Main.notify(msg);
+            this._log(msg);
+        }
+        this.last_status = status;
     }
 
     _onToggle(menuItem, state) {
         if (state) {
             let cmd = ["pkexec", "tee", "/sys/bus/pci/rescan"];
-            this._write_to_file(cmd);
-            let msg = 'Turned SoundCard On';
-            Main.notify(msg);
-            this._log(msg);
-            this._update_icon(state);
+            this._write_command(cmd);
         } else {
             let cmd = ["pkexec", "tee", "/sys/class/sound/card0/device/remove"];
-            this._write_to_file(cmd);
-            let msg = "Turned SoundCard Off";
-            Main.notify(msg);
-            this._log(msg);
-            this._update_icon(state);
+            this._write_command(cmd);
         }
     }
 });
